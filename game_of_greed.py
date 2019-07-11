934fe35209ab72b98137b2d6e19a1fe854808977
rgvh <rgvonhagel@gmail.com>
2019-07-11 15:00:55 -0700

    put original aside and started new file

--- game_of_greed.py
+++ game_of_greed.py
@@ -1,63 +1,33 @@
 # import random
 
 print("GAME OF GREED")
-'''
-Game of Greed objectives:
-Application should simulate rolling between 1 and 6 dice
-Application should allow user to set aside dice each roll
-Application should user to enter score per roll
-Application should allow “banking” current score or rolling again.
-Application should keep track of total score
-Application should keep track of current round
-track:
-current round
-number of dice being rolled
-score
-'''
+
 round_score = 0
 total_score = 0
 current_round = 0
 
-playing = input("Would you like to play?")
-if playing == 'y':
+print('Hit (x) to exit.')
+
+while current_round <= 10:
+
     current_round += 1
+    print("What was you score?")
+    add_score = input()
+    round_score = int(round_score) + int(add_score)
+    total_score = int(total_score) + int(round_score)
+    print(f'Your current score is {total_score}')
+    print("Do you want to (b)ank or (r)oll' again?")
+    next_action = input()
 
-    # if playing == 'n':
-    #     print("Sorry, you're missing out")
-    #     current_round = 0
-    while current_round >= 1:
-        print(f'Starting round {current_round}')
-        set_aside = 0
-        roll = []
-        print("What was you score?")
-        add_score = input()
-        round_score = int(round_score) + int(add_score)
-        total_score = int(total_score) + int(round_score)
-        print(f'Your current score is {total_score}')
+    if next_action == "r":
+        current_round += 1
 
-        print("Do you want to (b)ank or (r)oll' again?")
-        next_action = input()
+    if next_action == "b":
 
-        if next_action == "r":
-            current_round += 1
+        print(f'Your total score was: {total_score}')
 
-        if next_action == "b":
-
-            print(f'Your total score was: {total_score}')
 else:
     print("Game Over. Bye.")
 
-
-
-
-# def roll_dice(p)
-# for i in range(set_aside, 6):
-#     selected = random.randint(1, 6)
-#     roll.append(selected)
-# print(f'Your rolled: {roll}')
-    # current_round += 1
-
-# print("Which results do you wish to keep?")
-# new_aside = input()
-# set_aside = int(set_aside) + len(new_aside)
-# print(f'You have currently are holding {set_aside} die aside')
+if __name__ == "__main__":
+    # game_of_greed()
